be1aac60b32a2e13c2f913d71f7dfa1e182f86e5
Kyle Johnson <nukemknight@gmail.com>
2024-04-04 15:38:48 -0400

    Auto Updates

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,7 +27,7 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 5.5; // 4.8 theoretical max on High gearing.
+    public static final double kMaxSpeedMetersPerSecond = 5.0; // 4.8 theoretical max on High gearing.
                                                               //Upgraded to 5.5 for testing, max for Extra High 4 is 6.04m/sec
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
